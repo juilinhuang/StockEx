c037ed4b6a3f47f79eb8f99768702f30bf3ecfa9
kit-noel <leongrch@gmail.com>
2016-10-19 14:25:44 +0100

    login in account via accountmanager added

--- src/ie/StockEx/AccountManagement/AccountManager.java
+++ src/ie/StockEx/AccountManagement/AccountManager.java
@@ -2,4 +2,50 @@ package ie.StockEx.AccountManagement;
 
 public class AccountManager {
 
+	private static AccountManager INSTANCE;
+	
+	private int traderID;
+	
+	private int depotID;
+	
+	private AccountManager() {
+		traderID = 42; //TODO get from database
+		depotID = 0;
+	}
+	
+	public AccountManager getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new AccountManager();
+		}
+		
+		return INSTANCE;
+	}
+	
+	public Trader loginTrader(String username, String password) throws IllegalArgumentException {
+		if (username == null) {
+			throw new IllegalArgumentException("the given username is null.");
+		}
+		if (password == null) {
+			throw new IllegalArgumentException("the given password is null");
+		}
+		
+		Trader trader = null;
+
+		//TODO call database
+		
+		return trader;
+	}
+	
+	int getNextTraderID() {
+		traderID++;
+		
+		return traderID;
+	}
+	
+	int getNextDepotID() {
+		depotID++;
+		
+		return depotID;
+	}
+
 }
