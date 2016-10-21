10344c9384fd9552c48c5524583bc9812eacab60
kit-noel <leongrch@gmail.com>
2016-10-21 13:42:35 +0100

    single depot update factory method

--- src/ie/StockEx/AccountManagement/SingleDepot.java
+++ src/ie/StockEx/AccountManagement/SingleDepot.java
@@ -1,11 +1,24 @@
 package ie.StockEx.AccountManagement;
 
+import java.util.Map;
+
+import ie.StockEx.StockManagement.IFinancialProduct;
+
 public class SingleDepot extends Depot {
 	
 	private SingleTrader owner;
 
 	SingleDepot(int id) {
 		super(id);
+	}
+	
+	public static SingleDepot createSingleDepot(int id, double balance, double currentValue, Map<IFinancialProduct, Integer> assets) {
+		SingleDepot depot = new SingleDepot(id);
+		depot.balance = balance;
+		depot.currentValue = currentValue;
+		depot.assets = assets;
+		
+		return depot;
 	}
 	
 	public Trader getOwner() {
