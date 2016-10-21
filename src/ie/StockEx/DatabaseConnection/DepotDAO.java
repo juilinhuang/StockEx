8a6ebffe178a37cdd1673bddd70623d3491346d7
feiyasun <15067033@studentmail.ul.ie>
2016-10-21 17:55:29 +0100

    filedatabaseconnector

--- src/ie/StockEx/DatabaseConnection/DepotDAO.java
+++
@@ -1,9 +0,0 @@
-package ie.StockEx.DatabaseConnection;
-
-import java.io.IOException;
-
-public interface DepotDAO {
-
-	public abstract DepotDAOImpl selectDepotByDID(String id) throws IOException;
-	
-}
