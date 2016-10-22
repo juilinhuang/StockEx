ab8e1e0d334293daf0eedcf4cd6e799db922ed99
kit-noel <leongrch@gmail.com>
2016-10-22 20:20:37 +0100

    updating trader to implement the buyFuture function

--- src/ie/StockEx/AccountManagement/SingleTrader.java
+++ src/ie/StockEx/AccountManagement/SingleTrader.java
@@ -2,12 +2,9 @@ package ie.StockEx.AccountManagement;
 
 public class SingleTrader extends Trader {
 	
-	private TraderState state;
-
 	SingleTrader(Depot depot, int id) {
 		super(depot, id);
 		
 		state = new FreeState();
 	}
-
 }
--- src/ie/StockEx/AccountManagement/Trader.java
+++ src/ie/StockEx/AccountManagement/Trader.java
@@ -1,14 +1,26 @@
 package ie.StockEx.AccountManagement;
 
-public abstract class Trader {
+import java.util.Date;
+
+import ie.StockEx.StockExchangeConnection.StockExchangeConnector;
+import ie.StockEx.StockExchangeConnection.XetraConnector;
+import ie.StockEx.StockManagement.Future;
+import ie.StockEx.StockManagement.Stock;
+
+public abstract class Trader implements IBuyFinancialProducts {
 	
 	protected final Depot depot;
 	
 	private final int id;
 	
+	protected TraderState state;
+	
+	protected int tradesThisDay;
+	
 	Trader(Depot depot, int id) {
 		this.depot = depot;
 		this.id = id;
+		this.tradesThisDay = 0;
 	}
 	
 	public Depot getDepot() {
@@ -17,6 +29,24 @@ public abstract class Trader {
 	
 	public int getId() {
 		return id;
+	}
+	
+	@Override
+	public void buyStock(Stock stock) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void buyFuture(Stock stock, Date date) throws OutOfTradesException {
+		if (tradesThisDay > state.getTradesLimit()) {
+			throw new OutOfTradesException();
+		}
+		
+		StockExchangeConnector connector = new XetraConnector();
+		Future future = new Future(stock.getName() + "_Future", stock.getBuyTimePrice(), connector, stock, date);;
+	
+		depot.addAsset(future, 1);
 	}
 
 	@Override
