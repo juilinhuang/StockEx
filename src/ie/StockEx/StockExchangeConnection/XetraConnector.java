266ae015a73c1d6a5b09209cf80c2df78a79ffa2
kit-noel <leongrch@gmail.com>
2016-10-22 20:20:09 +0100

    adding xetra connector placeholder

---
+++ src/ie/StockEx/StockExchangeConnection/XetraConnector.java
@@ -0,0 +1,11 @@
+package ie.StockEx.StockExchangeConnection;
+
+public class XetraConnector extends StockExchangeConnector {
+
+	@Override
+	public double getCurrentPriceForStock(int stockId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
