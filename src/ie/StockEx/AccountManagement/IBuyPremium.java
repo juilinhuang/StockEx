5a92cc30e867f9803f0efcbc05d25088cf3b2e3f
kit-noel <leongrch@gmail.com>
2016-10-23 20:32:49 +0100

    buying premium interface added

---
+++ src/ie/StockEx/AccountManagement/IBuyPremium.java
@@ -0,0 +1,9 @@
+package ie.StockEx.AccountManagement;
+
+public interface IBuyPremium {
+	
+	public void buyPremiumYearly(String creditCard);
+	
+	void checkPremiumStatus();
+
+}
--- src/ie/StockEx/AccountManagement/SingleTrader.java
+++ src/ie/StockEx/AccountManagement/SingleTrader.java
@@ -1,10 +1,28 @@
 package ie.StockEx.AccountManagement;
 
-public class SingleTrader extends Trader {
+import java.time.LocalDateTime;
+
+public class SingleTrader extends Trader implements IBuyPremium {
+	
+	private LocalDateTime premiumExpiryDate;
 	
 	SingleTrader(Depot depot, int id) {
 		super(depot, id);
 		
 		state = new FreeState();
+	}
+
+	@Override
+	public void buyPremiumYearly(String creditCard) {		
+		state = new PremiumState();
+		LocalDateTime today = LocalDateTime.now();
+		premiumExpiryDate = today.plusYears(1);
+	}
+
+	@Override
+	public void checkPremiumStatus() {
+		if (premiumExpiryDate.isBefore(LocalDateTime.now())) {
+			state = new FreeState();
+		}
 	}
 }
