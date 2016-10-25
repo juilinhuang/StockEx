c5b5c7eba43211c88db3b9ba133d35c06c8b7106
kit-noel <leongrch@gmail.com>
2016-10-25 21:14:37 +0100

    removin unncessary comment

--- src/ie/StockEx/AccountManagement/OutOfTradesException.java
+++ src/ie/StockEx/AccountManagement/OutOfTradesException.java
@@ -2,9 +2,6 @@ package ie.StockEx.AccountManagement;
 
 public class OutOfTradesException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -7228245873220923600L;
 
 }
