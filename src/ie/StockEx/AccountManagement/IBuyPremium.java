980dfbeb1295c878da2cb175196a3828155f86ce
kit-noel <leongrch@gmail.com>
2016-10-25 21:14:09 +0100

    success return value for buying premium added

--- src/ie/StockEx/AccountManagement/IBuyPremium.java
+++ src/ie/StockEx/AccountManagement/IBuyPremium.java
@@ -2,7 +2,7 @@ package ie.StockEx.AccountManagement;
 
 public interface IBuyPremium {
 	
-	public void buyPremiumYearly(String creditCard);
+	public boolean buyPremiumYearly(String creditCard);
 	
 	void checkPremiumStatus();
 
