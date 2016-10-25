61c70c80090cc1ca1c52163cfde5ab6bb46189ee
kit-noel <leongrch@gmail.com>
2016-10-25 21:14:23 +0100

    quantity for buying futures

--- src/ie/StockEx/AccountManagement/IBuyFinancialProducts.java
+++ src/ie/StockEx/AccountManagement/IBuyFinancialProducts.java
@@ -8,5 +8,5 @@ public interface IBuyFinancialProducts {
 
 	public void buyStock(Stock stock) throws OutOfTradesException;
 	
-	public void buyFuture(Stock stock, Date date) throws OutOfTradesException;
+	public void buyFuture(Stock stock, Date date, int quantity) throws OutOfTradesException;
 }
