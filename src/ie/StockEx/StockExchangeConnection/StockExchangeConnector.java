9fb6278d01eeec4e6fed663619c8c2f463e430ec
kit-noel <leongrch@gmail.com>
2016-10-25 18:36:23 +0100

    make connector observable

--- src/ie/StockEx/StockExchangeConnection/StockExchangeConnector.java
+++ src/ie/StockEx/StockExchangeConnection/StockExchangeConnector.java
@@ -1,6 +1,8 @@
 package ie.StockEx.StockExchangeConnection;
 
-public abstract class StockExchangeConnector extends Thread {
+import java.util.Observable;
+
+public abstract class StockExchangeConnector extends Observable implements Runnable{
 
 	public abstract double getCurrentPriceForStock(int stockId);
 }
