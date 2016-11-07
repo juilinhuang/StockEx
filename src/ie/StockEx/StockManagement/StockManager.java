00662efdf51f70ecc1891c57c167e2e03ce65566
kit-noel <leongrch@gmail.com>
2016-11-07 20:12:53 +0000

    removed useless code

--- src/ie/StockEx/StockManagement/StockManager.java
+++ src/ie/StockEx/StockManagement/StockManager.java
@@ -1,38 +1,19 @@
 package ie.StockEx.StockManagement;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class StockManager {
 
 	private static StockManager INSTANCE;
 	
 	private int productID;
 	
-	private Map<String, Stock> stocks;
-	
 	private StockManager() {
 		this.productID = 0;
-		this.stocks = new HashMap<String, Stock>();
 	}
 	
 	synchronized int getNextProductID() {
 		productID++;
 		
 		return productID;
-	}
-	
-	public Stock getStock(String name) {
-		if (name == null) {
-			throw new IllegalArgumentException("the given name is not initialized");
-		}
-		
-		Stock result = stocks.get(name);
-		if (result == null) {
-			
-		}
-		
-		return result;
 	}
 	
 	static StockManager getInstance() {
